19e267788aed39382392cc7206118e9ea65c8c00
ben b <benwbethell@gmail.com>
2025-06-30 16:56:58 +0100

    vocab loaded

--- README.md
+++ README.md
@@ -22,9 +22,9 @@ Inference:
 ![A6098B6E-7365-497E-A0B9-20E573ED6993](https://github.com/user-attachments/assets/7fa56d04-5c6c-4963-b960-1071c5cd63c2)
 
 1. Download the hugging face Flickr dataset
-2. Link it up with CLIP VIT Encoder (this already has positional encoding embedding and patching - 32pixel x 32pixel = 1 patch by default)
+2. Link it up with CLIP ViT Encoder (this already has positional encoding embedding and patching - 32pixel x 32pixel = 1 patch by default)
     - Might need to review: 3 colour channels
-    - Deal with sizing (photos are different sizes - max of both width and height 470pixel x 500pixel)
+    - Deal with sizing (photos are different sizes - max of both width and height 470pixel x 500pixel) 
 3. Extract one (out of 5) caption -> word2vec -> to vector
     - Also handling different lengths (train_datset max length 402 - test_datasest 342)
 4. Concat `<start token> + <vectors of patches> + <vectors of caption> + <end token>`
--- src/model/dummy.py
+++ src/model/dummy.py
@@ -1,9 +1,35 @@
 import transformers
+from utils import get_device
+import clip
 
-c = transformers.CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+device = get_device()
+
+clip_model = transformers.CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
 params = lambda m: sum(p.numel() for p in m.parameters())
 
-print(f"CLIP model has {params(c)} parameters.")
+print(f"CLIP model has {params(clip_model)} parameters.")
 # Output: CLIP model has 152,897,536 parameters.
 
-print("CLIP: ", c)
+print("CLIP: ", clip_model)
+
+# switch model to eval mode 
+clip_model.eval()
+
+# load model from huggingface 
+clip_processor = transformers.CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+tokenizer = clip_processor.tokenizer
+vocab = tokenizer.get_vocab()
+
+print(type(vocab)) # should be class dict 
+print(len(vocab)) # should be 49408 vocab 
+
+
+
+
+
+
+
+# model, preprocess = clip.load("ViT-B/32", device=device)
+
+# avail_models = clip.available_models()
+
