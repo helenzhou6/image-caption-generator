228aa21b391968ed2bc2efecf56a6a8932152e45
Helen <25727036+helenzhou6@users.noreply.github.com>
2025-06-30 14:35:34 +0000

    Start pandas processing

--- src/model/dataset.py
+++ src/model/dataset.py
@@ -1,16 +1,19 @@
 from datasets import load_dataset
 import os
+import pandas as pd
 
 # Load the Flickr30k dataset
 dataset = load_dataset("nlphuji/flickr30k", split="test")
+pd_dataset = pd.DataFrame(dataset).head(100)
 
-# Now split based on the 'split' column
-train_dataset = dataset.filter(lambda x: x["split"] == "train")
+train_dataset = pd_dataset[pd_dataset["split"] == "train"]
 
 os.makedirs("data", exist_ok=True)
-train_dataset.to_parquet("data/train.parquet")
-sample = train_dataset[0]
-print(sample)
 
+print(train_dataset.head())
+# train_dataset.to_parquet("data/train.parquet", engine="pyarrow")
+
+# sample = train_dataset[0]
+# print(sample)
 # Output = {'image': <PIL.JpegImagePlugin.JpegImageFile image mode=RGB size=333x500 at 0x722F4274AB60>,
 #  'caption': ['Two young guys with shaggy hair look at their hands while hanging out in the yard.', 'Two young, White males are outside near many bushes.', 'Two men in green shirts are standing in a yard.', 'A man in a blue shirt standing in a garden.', 'Two friends enjoy time spent together.'], 'sentids': ['0', '1', '2', '3', '4'], 'split': 'train', 'img_id': '0', 'filename': '1000092795.jpg'}
